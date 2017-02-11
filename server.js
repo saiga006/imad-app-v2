3f870abdc35b55b47243d61e8e0807e6de2c5604
saiga006 <sai.mukkundan@gmail.com>
2017-02-11 21:41:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/1408639964981.jpg', function(req,res){
+app.get('/ui/1408639964981.jpg', function(req,res) {
    res.sendFile(path.join(_dirname,'ui','1408639964981.jpg'));  
 });
-app.get('/chrom',function(req,res)
-{res.sendFile('chrom will be served here');
+app.get('/chrom',function(req,res) {
+    res.sendFile('chrom will be served here');
     
 });
 
