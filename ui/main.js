71a2bd5513dcef7c431862f107ec2cfca95283fa
saiga006 <sai.mukkundan@gmail.com>
2017-02-12 12:27:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,13 @@ console.log('my webpage is deployed');
 // changing the html content by using javascript
 var elem=document.getElementById('new');// getting the text
 var elem2=document.getElementById('imag');//getting my image
-elem.innerHTML="changed the text using javascript";
+elem.innerHTML="changed the text using javascript"; // changes the text content
 var margi1=0; 
 function moveRight () {
-    margi1=margi1+40;
+    margi1=margi1+40;                //increments the left margin by 40px for each call
 elem2.style.marginLeft=margi1 +'px';    
 }
 elem2.onclick=function() //function executes on clicking the image 
 {
-  var interval= setInterval(moveRight,100);
- // elem2.style.marginRight='120px';  
+  var interval= setInterval(moveRight,100);//moves the image right by increasing the margin left for each 100ms calls the moveright function
 };
