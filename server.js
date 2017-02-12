d73ea407f212131b7209f7d3cda30a598347446a
saiga006 <sai.mukkundan@gmail.com>
2017-02-12 11:51:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,9 @@ app.get('/ui/1408639964981.jpg', function(req,res) {
 });
 app.get('/chrom',function(req,res) {
     res.send(createTemplate(chro));
-    
+});
+app.get('/ui/main.js',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','main.js'));  
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
