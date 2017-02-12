2ae84ad2d2cba4e9a4ed1c62548b3c36df0f774b
saiga006 <sai.mukkundan@gmail.com>
2017-02-12 17:13:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,18 @@ request.onreadystatechange=function () {
 request.open('GET','http://saiga006.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+//submit name
+var namInput=document.getElementById('name');
+var nam2=namInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function (){
+  //make a request to server and send the same
+  //capture a list of names and render it as list
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for (var i=0;i<names.length;i++) {
+      list +='<li>'+names[i]+'</li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+};
